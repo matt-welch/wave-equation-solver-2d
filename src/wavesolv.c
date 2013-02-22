4eede3b5c4a57e5b71af53a82326416abee88c44
Matt Welch <matt-welch@users.noreply.github.com>
2013-02-22 11:36:11 -0700

    Timing and memory allocation for initial arrays working. U-arrays now fill and propagate.  Modified Makefile to check dependencies.  TODO: add pulses.

--- src/wavesolv.c
+++ src/wavesolv.c
@@ -11,23 +11,25 @@
 
 # include <stdio.h>  /* printf*/
 # include <stdlib.h>
+# include <string.h> /* memset */
 # include <math.h>   
 # include <omp.h>
 # include <time.h>
 # include <sys/time.h>
+
 
 /* function to check Courant-Friedrichs-Lewy condition 
  * for the wave equation solver to be stable, the value of c 
  * should be less than 1 */
 double checkCFL(int dx, int dy, int dt){ 
     double dbl_dt = (double)dt;
-    return (dbl_dt/(double)dx * dbl_dt/(double)dy); 
+    return (dbl_dt/(double)dx + dbl_dt/(double)dy); 
 }
 
 int getNextValue(int u1, int u0, int u1e, int u1s, int u1w, int u1n, int r){
-    /* u(l-1,i,j)       = u0
+    /* u(l-1,i,j)       = u0      (last center)
     *
-     * u(l,i,j)         = u1
+     * u(l,i,j)         = u1      (center)
      * u(l,i+1,j)       = u1e     (east)
      * u(l,i,j+1)       = u1n     (north)
      * u(l,i-1,j)       = u1w     (west)
@@ -35,25 +37,55 @@ int getNextValue(int u1, int u0, int u1e, int u1s, int u1w, int u1n, int r){
      * 
      * u(l+1,i,j)       = u2        (solving for this)
     */
-    return(2 * u1 - u0 + r*r*(u1e + u1w + u1n + u1s - 4 * u1));
+    return( 2*u1 - u0 + r*r*(u1e + u1w + u1n + u1s - 4*u1) );
+}
+
+void allocateArray(int **array, int domSize){
+	/* function takes a square input array and zeroes its contents 
+	 * assumes that the array has NOT YET been allocated 
+	 * TODO: consider using contiguous memory to allocate the array*/ 
+	int i;
+	array = malloc(domSize * sizeof(int *) );
+	if(array == NULL){
+		printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+	}
+	for(i=0; i < domSize; ++i){
+		array[i] = malloc(domSize * sizeof(int));
+		if(array == NULL){
+			printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+		}
+	}
+}
+
+void freeArrays(int **array, int domSize){
+	/* function takes a square input array and zeroes its contents 
+	 * assumes that the array has NOT YET been allocated */ 
+	int i;
+	for(i=0; i < domSize; ++i){
+		free(array[i]);
+	}
+	free(array);
 }
 
 int main(int argc, char* argv[]) {
     /* declare global variables */
-    unsigned short t, x, y;  /* declare t, x, y as shorts to index the domain */
+    /* unsigned short t, x, y; */
+	/* declare t, x, y as shorts to index the domain */
     unsigned short dt, dx, dy; /* step size for t, x, y*/
-    unsigned short tmax, xmax, ymax;  
+    unsigned short tmax, xmax, ymax, domSize;  
     
     /* u is the wave magnitude as an int for 32-bits of accuracy 
-     * u is an array representing the magnitude across the whole domain 
-     * u0 is the */
+     * u0 is the array representing the domain for iteration l-1
+     * u1 is the array representing the domain for iteration l
+     * u2 is the array representing the domain for iteration l+1
+     */
     int ** u0;
     int ** u1;
-    int ** um1; 
+    int ** u2; 
     
-    /* c is the wave velocity 
+    /* c is the wave velocity - unused  
      * r is a coefficient in the wave equation where r = dt/dx' */
-    double c, r;
+    double r;
     
     /* index variables for loops */
     short i, j, l; 
@@ -66,28 +98,96 @@ int main(int argc, char* argv[]) {
     long seconds, useconds;
     double preciseTime;
 
+
     /* Get start time */
     gettimeofday(&start, NULL);
     
-    /* duration of simulation, width, and height of domain */ 
+	/* duration of simulation(steps), width, and height of domain */ 
     tmax = 100;
-    xmax = 480; 
-    ymax = 480;
+    domSize = xmax = ymax = 480 + 2; /* two greater than 480 for ghost rows*/ 
+	
+	/* step sizes for t, x, & y*/
+	dt = 4;
+	dx = dy = 9;
+	CFL = checkCFL(dx, dy, dt);
+	r = (double)dt/(double)dx;
 
+	printf("CFL = %3.3f\n", CFL);
+   
+	/* allocate memory for arrays */
+	/* u0 = u(l-1)*/
+	u0 = malloc(domSize * sizeof(int *) );
+	if(u0 == NULL){
+		printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+	}
+	for(i=0; i < domSize; ++i){
+		u0[i] = malloc(domSize * sizeof(int));
+		if(u0 == NULL){
+			printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+		}else{
+			memset(u0[i], 0,domSize); 
+		}
+	}
+	/* u1 = u(l)*/
+	u1 = malloc(domSize * sizeof(int *) );
+	if(u1 == NULL){
+		printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+	}
+	for(i=0; i < domSize; ++i){
+		u1[i] = malloc(domSize * sizeof(int));
+		if(u1 == NULL){
+			printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+		}else{
+			memset(u1[i], 0,domSize); 
+		}
+	}
+	/* u2 = u(l+1)*/
+	u2 = malloc(domSize * sizeof(int *) );
+	if(u2 == NULL){
+		printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+	}
+	for(i=0; i < domSize; ++i){
+		u2[i] = malloc(domSize * sizeof(int));
+		if(u2 == NULL){
+			printf("Error: malloc failed to allocate %lu bytes for array\n", domSize * sizeof(int));
+		}else{
+			memset(u2[i], 0,domSize); 
+		}
+	}
     /* Initialize MPI */
 
     /* loop through time at single step intervals */
     for(l = 0; l < tmax; ++l){ 
-        /* loop through x and y to calculate u at each point in the domain */
-        for(i = 0; i < xmax; ++i){ 
-            for(j = 0; j < ymax; ++j){ 
-
-
+        /* update u - loop through x and y to calculate u at each point in the domain */
+        for(i = 1; i < (xmax-1); ++i){ 
+            for(j = 0; j < (ymax-1); ++j){ 
+				u2[i][j] = getNextValue(u1[i][j], u0[i][j], u1[i+1][j], u1[i][j+1], u1[i-1][j], u1[i][j-1], r);
             }
-        }            
+        }
+
+
+		/* if maximum magnitude has reached 20% of initial pulse magnitude,
+		 * introduct another pulse at next edge*/
     }
 
     /* finalize MPI */
+
+	/* free memory for arrays */
+	/* free memory for u0 */
+	for(i=0; i < domSize; ++i){
+		free(u0[i]);
+	}
+	free(u0);
+	/* free memory for u1 */
+	for(i=0; i < domSize; ++i){
+		free(u1[i]);
+	}
+	free(u1);
+	/* free memory for u2 */
+	for(i=0; i < domSize; ++i){
+		free(u2[i]);
+	}
+	free(u2);
 
     /* timekeeping */ 
     gettimeofday(&end, NULL);
